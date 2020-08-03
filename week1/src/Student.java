5fc3ba8e3a6684bf767ae0eb326ec42ac7dac712
Dungnt.uet <ntd036098002094@gmail.com>
2020-08-03 12:10:57 +0700

    update week 1
    khoi tao lop student
    khoi tao lop studentmanagement

---
+++ week1/src/Student.java
@@ -0,0 +1,62 @@
+
+public class Student {
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getGroup(){
+        return this.group;
+    }
+    public void setGroup(String name){
+        this.group = group;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+//    public  Student(){
+//        this.name = "Nguyen Van An";
+//        this.id = "17020001";
+//        this.group = "K62CC";
+//        this.email = "17020001@vnu.edu.vn";
+//    }
+    public String getInfo(){
+        return (name+" - "+id+" - "+group+" - "+email);
+    }
+    public  Student(){
+        this.name = "Student";
+        this.id = "0000";
+        this.group = "K62CB";
+        this.email = "uet@vnu.edu.vn";
+    }
+
+    public Student(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.group = "K62CB";
+        this.email = email;
+    }
+
+    public Student (Student s){
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
+    }
+}
+
---
+++ week1/src/StudentManagement.java
@@ -0,0 +1,7 @@
+public class StudentManagement {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Nguyen Van An", "17021111","17021111@vnu.edu.vn");
+        System.out.println(s1.getInfo());
+    }
+}
